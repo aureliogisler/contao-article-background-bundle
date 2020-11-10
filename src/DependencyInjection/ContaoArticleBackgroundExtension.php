65fa4412452464574bda8392a4696e70aeadf81c
xippoGmbH <aurelio.gisler@xippo.ch>
2020-11-10 18:01:04 +0100

    DepencyIncection

---
+++ src/DependencyInjection/ContaoArticleBackgroundExtension.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * This file is part of a Xippo GmbH Contao Article Background Bundle.
+ *
+ * (c) Aurelio Gisler (Xippo GmbH)
+ *
+ * @author     Aurelio Gisler
+ * @package    ContaoArticleBackground
+ * @license    MIT
+ * @see        https://github.com/xippoGmbH/contao-article-background-bundle
+ *
+ */
+
+namespace XippoGmbH\ContaoArticleBackgroundBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+class ContaoArticleBackgroundExtension extends Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $mergedConfig, ContainerBuilder $container)
+    {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__.'/../Resources/config')
+        );
+
+        $loader->load('services.yml');
+    }
+}
